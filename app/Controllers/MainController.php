ba77b7557f5bb29c4ffbb4c3507b33af9e8e632a
nourhan <nourhan_elshenawy@hotmail.com>
2016-07-25 08:21:50 +0300

    successfully display recent pictures of authenticated user

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -21,18 +21,72 @@ class MainController extends Controller
 //        ini_set('default_socket_timeout', 5);
 //        session_start();
 
-        $clientID = '30cb4af47d4545f7b96aac046f59b157';
-        $clientSecret = '38a96dc94cc940b8bf9bb3554d0e07de';
-        $redirectURI = 'http://test.app';
-        $ImageDirectory = 'pics/';
+        define('clientID', '30cb4af47d4545f7b96aac046f59b157');
+        define('clientSecret', '38a96dc94cc940b8bf9bb3554d0e07de');
+        define('redirectURI', 'http://test.app');
+        define('ImageDirectory', 'pics/');
+//        $clientID = '30cb4af47d4545f7b96aac046f59b157';
+//        $clientSecret = '38a96dc94cc940b8bf9bb3554d0e07de';
+//        $redirectURI = 'http://test.app';
+//        $ImageDirectory = 'pics/';
+
+
+        echo $this->twig->render('index.twig', array('clientID'=>clientID, 'client_Secret'=>clientSecret, 'redirectURI'=>redirectURI, 'ImageDirectory'=>ImageDirectory));
+
+        function connectToInstagram($url){
+            $ch = curl_init();
+
+            curl_setopt_array($ch, array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_SSL_VERIFYPEER => false,
+                CURLOPT_SSL_VERIFYHOST => 2,
+            ));
+
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+            return $result;
+        }
+
+        function getImages($userID, $access_token){
+            $url = "https://api.instagram.com/v1/users/".$userID."/media/recent/?access_token=".$access_token;
+//            $url = "https://api.instagram.com/v1/users/".$userID."/media/recent/?access_token=".$access_token."&count=5";
+            $instagramInfo = connectToInstagram($url);
+            $results = json_decode($instagramInfo, true);
+
+            var_dump($instagramInfo);
+
+            foreach($results['data'] as $item){
+                $image_url = $item['images']['low_resolution']['url'];
+                echo '<img src=" '.$image_url.' "/> <br/>';
+                var_dump($item);
+            }
+        }
+
+//        function getTagImages($tag, $userID, $access_token){
+////            $url = "https://api.instagram.com/v1/users/".$userID."/media/recent/?access_token=".$access_token;
+////            $url = "https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=".$access_token;
+////            $url = 'https://api.instagram.com/v1/tags/'.$tag.'/media/recent?client_id='.$userID.'/?access_token='.$access_token;
+//            $url = "https://api.instagram.com/v1/users/".$userID."/tags/".$tag."/media/recent?access_token=".$access_token;
+//            $instagramInfo = connectToInstagram($url);
+//            $results = json_decode($instagramInfo, true);
+//
+//            var_dump($instagramInfo);
+//
+////            foreach($results['data'] as $item){
+////                $image_url = $item['images']['low_resolution']['url'];
+////                echo '<img src=" '.$image_url.' "/> <br/>';
+////            }
+//        }
 
         if( isset($_GET['code']) ) {
             $code = $_GET['code'];
             $url = "https://api.instagram.com/oauth/access_token";
-            $access_token_settings = array('clientID' => $clientID,
-                'clientSecret' => $clientSecret,
-                'grantType' => 'authorization_code',
-                'redirectURI' => $redirectURI,
+            $access_token_settings = array('client_id' => clientID,
+                'client_secret' => clientSecret,
+                'grant_type' => 'authorization_code',
+                'redirect_uri' => redirectURI,
                 'code' => $code);
 
             $curl = curl_init($url);
@@ -41,9 +95,18 @@ class MainController extends Controller
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
-        }
+            $result = curl_exec($curl);
+            curl_close($curl);
 
-        echo $this->twig->render('index.twig', array('clientID'=>$clientID, 'client_Secret'=>$clientSecret, 'redirectURI'=>$redirectURI, 'ImageDirectory'=>$ImageDirectory));
+            $results = json_decode($result, true);
+            var_dump($results);
+//            echo $results['user']['username'];
+            $userID =  $results['user']['id'];
+            $access_token = $results['access_token'];
+            echo "ID: ".$userID;
+            getImages($userID, $access_token);
+//            getTagImages("nofilter", $userID, $access_token);
+        }
     }
 
     public function contact()
