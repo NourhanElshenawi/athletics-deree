4f6f16125870ceef7a56ddfea572ea45751cd801
nourhan <nourhan_elshenawy@hotmail.com>
2016-08-08 09:20:16 +0300

    initial commit

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -21,18 +21,16 @@ class MainController extends Controller
 //        ini_set('default_socket_timeout', 5);
 //        session_start();
 
+        // API Info
         define('clientID', '30cb4af47d4545f7b96aac046f59b157');
         define('clientSecret', '38a96dc94cc940b8bf9bb3554d0e07de');
         define('redirectURI', 'http://test.app');
         define('ImageDirectory', 'pics/');
-//        $clientID = '30cb4af47d4545f7b96aac046f59b157';
-//        $clientSecret = '38a96dc94cc940b8bf9bb3554d0e07de';
-//        $redirectURI = 'http://test.app';
-//        $ImageDirectory = 'pics/';
 
 
         echo $this->twig->render('index.twig', array('clientID'=>clientID, 'client_Secret'=>clientSecret, 'redirectURI'=>redirectURI, 'ImageDirectory'=>ImageDirectory));
 
+        // to connect to the api whenever we want, example: every time we request recent pictures
         function connectToInstagram($url){
             $ch = curl_init();
 
@@ -49,8 +47,10 @@ class MainController extends Controller
             return $result;
         }
 
+        //get the recent pictures of the authenticated user
         function getImages($userID, $access_token){
             $url = "https://api.instagram.com/v1/users/".$userID."/media/recent/?access_token=".$access_token;
+            //for a specific number of pictures use the url below
 //            $url = "https://api.instagram.com/v1/users/".$userID."/media/recent/?access_token=".$access_token."&count=5";
             $instagramInfo = connectToInstagram($url);
             $results = json_decode($instagramInfo, true);
@@ -64,21 +64,6 @@ class MainController extends Controller
             }
         }
 
-//        function getTagImages($tag, $userID, $access_token){
-////            $url = "https://api.instagram.com/v1/users/".$userID."/media/recent/?access_token=".$access_token;
-////            $url = "https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=".$access_token;
-////            $url = 'https://api.instagram.com/v1/tags/'.$tag.'/media/recent?client_id='.$userID.'/?access_token='.$access_token;
-//            $url = "https://api.instagram.com/v1/users/".$userID."/tags/".$tag."/media/recent?access_token=".$access_token;
-//            $instagramInfo = connectToInstagram($url);
-//            $results = json_decode($instagramInfo, true);
-//
-//            var_dump($instagramInfo);
-//
-////            foreach($results['data'] as $item){
-////                $image_url = $item['images']['low_resolution']['url'];
-////                echo '<img src=" '.$image_url.' "/> <br/>';
-////            }
-//        }
 
         if( isset($_GET['code']) ) {
             $code = $_GET['code'];
@@ -91,21 +76,19 @@ class MainController extends Controller
 
             $curl = curl_init($url);
             curl_setopt($curl, CURLOPT_POST, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $access_token_settings);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $access_token_settings);  // setting up
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // true if we go live
 
             $result = curl_exec($curl);
             curl_close($curl);
 
             $results = json_decode($result, true);
             var_dump($results);
-//            echo $results['user']['username'];
             $userID =  $results['user']['id'];
             $access_token = $results['access_token'];
             echo "ID: ".$userID;
             getImages($userID, $access_token);
-//            getTagImages("nofilter", $userID, $access_token);
         }
     }
 
