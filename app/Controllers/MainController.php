b6d09f2385f2d89a01a1c469aefef419bf3160b3
nourhan <nourhan_elshenawy@hotmail.com>
2016-11-20 02:59:35 +0200

    add guidance headers for stats

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -155,8 +155,8 @@ class MainController extends Controller
     public function verifyResponse()
     {
 // your secret key
-        $secret = "6Lel3ggUAAAAACT3Xz7dfhUhvFGALwDrXgtwFeON";
-//        $secret = "6Ld91QgUAAAAAIRUcZnJqHYO836O9dCCesrdMgLg"; //for productions
+//        $secret = "6Lel3ggUAAAAACT3Xz7dfhUhvFGALwDrXgtwFeON";
+        $secret = "6Ld91QgUAAAAAIRUcZnJqHYO836O9dCCesrdMgLg"; //for productions
 // empty response
         $response = null;
 // check secret key
