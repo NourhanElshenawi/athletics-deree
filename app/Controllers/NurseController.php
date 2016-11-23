d5425ff3fb201fe092a3ca213b49b8a8d3c88c9d
nourhan <nourhan_elshenawy@hotmail.com>
2016-11-23 13:20:13 +0200

    pulling from sony

--- app/Controllers/NurseController.php
+++ app/Controllers/NurseController.php
@@ -46,7 +46,28 @@ class NurseController extends Controller
         $db = new DB();
 
         $result = $db->approveUserCertificate($_POST['user_certificate_id']);
+        if($result){
+//            // the message
+//            $msg = "Dear ". $_SESSION['user']['name'].",\n".
+//                "We would like to inform you that your Dr. Certificate has been approved!\n".
+//                "You can now use all the athletics facilities";
+//
+//            // use wordwrap() if lines are longer than 70 characters
+//            $msg = wordwrap($msg,70);
+//
+//            // send email
+//            mail("n.elshenawi@acg.edu","Dr. Certificate Approved!",$msg);
 
+            $to = "n.elshenawi@acg.edu";
+            $subject = "Dr. Certificate Approved!";
+            $txt = "Dear ". $_SESSION['user']['name'].",\n".
+                    "We would like to inform you that your Dr. Certificate has been approved!\n".
+                    "You can now use all the athletics facilities";
+            $headers = "From: nourhanelshenawy@gmail.com" . "\r\n" .
+                "CC: nourhan_elshenawy@hotmail.com";
+
+            mail($to,$subject,$txt,$headers);
+        }
         echo json_encode($result);
     }
 
