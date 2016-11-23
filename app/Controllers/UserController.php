778195a7e769b735ee80d6f019a363f355433a6b
Nourhan Elshenawi <n.elshenawi@acg.edu>
2016-11-23 09:13:04 +0200

    push to heroku

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -49,8 +49,8 @@ class UserController extends Controller
     public function verifyResponse()
     {
 // your secret key
-        $secret = "6Lel3ggUAAAAACT3Xz7dfhUhvFGALwDrXgtwFeON";
-//        $secret = "6Ld91QgUAAAAAIRUcZnJqHYO836O9dCCesrdMgLg"; //for productions
+//        $secret = "6Lel3ggUAAAAACT3Xz7dfhUhvFGALwDrXgtwFeON";
+        $secret = "6Ld91QgUAAAAAIRUcZnJqHYO836O9dCCesrdMgLg"; //for productions
 // empty response
         $response = null;
 // check secret key
